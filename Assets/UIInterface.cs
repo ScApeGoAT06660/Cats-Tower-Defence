8d967336d810b29c9ac76df8cc409f9f6f3df5c3
ScApeGoAT666 <julitamajer@gmail.com>
2025-03-10 19:09:46 +0100

    CATS-10 && CATS-7
    Turrets placement, upgrade menu, basic UI

--- Assets/UIInterface.cs
+++ Assets/UIInterface.cs
@@ -1,32 +1,74 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
 
 public class UIInterface : MonoBehaviour
 {
-    public GameObject tower;
+    public GameObject rocketTurret;
+    public GameObject gattlingTurret;
+    public GameObject flammerTurret;
+
+    public GameObject turretMenu;
+
+    GameObject itemPrefab;
     GameObject focusObj;
-    void Start()
+
+    public void CreateRocket()
     {
-        
+        itemPrefab = rocketTurret;
+        CreateItemForButton();
     }
 
-
-    void Update()
+    public void CreateFlammer()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
+        itemPrefab = flammerTurret;
+        CreateItemForButton();
+    }
+
+    public void CreateGasttling()
+    {
+        itemPrefab = gattlingTurret;
+        CreateItemForButton();
+    }
+
+    void CreateItemForButton()
+    {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (!Physics.Raycast(ray, out hit))
                 return;  
 
-            focusObj = Instantiate(tower, hit.point, tower.transform.rotation);
+            focusObj = Instantiate(itemPrefab, hit.point, itemPrefab.transform.rotation);
             focusObj.GetComponent<Collider>().enabled = false;
+    }
 
+    public void CloseTurretMenu()
+    {
+        turretMenu.SetActive(false);
+    }
 
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hit))
+            {
+                if (hit.collider.tag == "Turret")
+                {
+                    turretMenu.transform.position = Input.mousePosition;
+                    turretMenu.SetActive(true);
+                }
+            }
         }
-        else if (Input.GetMouseButton(0))
+        else if (focusObj && Input.GetMouseButton(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -34,10 +76,9 @@ public class UIInterface : MonoBehaviour
             if (!Physics.Raycast(ray, out hit))
                 return;
 
-            focusObj.transform.position = hit.point + new Vector3(0, 0.5f, 0);
-
+            focusObj.transform.position = hit.point;
         }
-        else if (Input.GetMouseButtonUp(0))
+        else if (focusObj && Input.GetMouseButtonUp(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -45,15 +86,20 @@ public class UIInterface : MonoBehaviour
             if (Physics.Raycast(ray, out hit) && 
                 hit.collider.gameObject.CompareTag("Platform"))
             {
+                Vector3 newPosition = hit.collider.gameObject.transform.position;
+                newPosition.y += 1f; 
+                focusObj.transform.position = newPosition;
+
                 hit.collider.gameObject.tag = "Occupied";
+
+                focusObj.GetComponent<Collider>().enabled = true;
             }
             else
             {
                 Destroy(focusObj);
             }
                 
-
+            focusObj = null;
         }
-
     }
 }
